d59b93da76f57da6c37416145ffb4ea1a159354f
SadhakKumar <sadhak2003@gmail.com>
2023-03-12 19:42:28 +0530

    add tree and circularqueue

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -8,6 +8,11 @@ using TMPro;
 public class NewBehaviourScript : MonoBehaviour
 {
     public LineRenderer line;
+    public LineRenderer line2;
+    public LineRenderer line3;
+    public LineRenderer line4;
+    public LineRenderer line5;
+    public LineRenderer line6;
     public GameObject TreeNode0;
     public GameObject TreeNode1;
     public GameObject TreeNode2;
@@ -17,15 +22,26 @@ public class NewBehaviourScript : MonoBehaviour
     public GameObject TreeNode6;
     public string input;    
     public TextMeshProUGUI inputField;
+    public TextMeshProUGUI preordertext;
+    public TextMeshProUGUI inordertext;
+    public TextMeshProUGUI postordertext;
+
+    public Material material;
     // public Transform posi1;
     // public Transform posi2;
     
     List<GameObject> unityGameObjects = new List<GameObject>();
     int index = 0;
+    int z = 0;
     // Start is called before the first frame update
     void Start()
     {
-        line.positionCount = 3;
+        // line.enabled = false;
+        // line2.enabled = false;
+        // line3.enabled = false;
+        // line4.enabled = false;
+        // line5.enabled = false;
+        // line6.enabled = false;
         unityGameObjects.Add(TreeNode0);
         unityGameObjects.Add(TreeNode1);
         unityGameObjects.Add(TreeNode2);
@@ -36,8 +52,8 @@ public class NewBehaviourScript : MonoBehaviour
         Debug.Log(unityGameObjects);  
         Debug.Log(unityGameObjects.Count);
         Debug.Log("hello world");
-        // LineRenderer.startColor = Color.red;
-        // LineRenderer.endColor = Color.red;
+        // line.startColor = Color.blue;
+        // line.endColor = Color.blue;
 
         // set width of the renderer
         // LineRenderer.startWidth = 0.3f;
@@ -55,17 +71,46 @@ public class NewBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
         if(Input.GetKeyDown(KeyCode.Space)){
+            if(index == 1){
+                // line.enabled = true;
+                line.material = material;
+
+            }
+            if(index == 2){   
+                // line2.enabled = true;
+                line2.material = material;
+
+            }
+            if(index == 3){
+                line3.material = material;
+                line3.enabled = true;
+   
+            }
+            if(index == 4){
+                line4.material = material;
+                line4.enabled = true;
+   
+            }
+            if(index == 5){
+                line5.material = material;
+                line5.enabled = true;
+   
+            }
+            if(index == 6){
+                line6.material = material;
+                line6.enabled = true;
+                // traversal();
+   
+            }
             if(index < 7){
                 unityGameObjects[index].SetActive(true);
                 unityGameObjects[index].GetComponentInChildren<TextMeshPro>().text = input.ToString();
                 index++;
             }
              
-            if(index == 1 || index == 2){
-                line.SetPosition(0, unityGameObjects[0].transform.position);
-                line.SetPosition(1, unityGameObjects[index].transform.position);
-            }
+            
            
             
         }
@@ -77,6 +122,83 @@ public class NewBehaviourScript : MonoBehaviour
     public void GetInput(string s){
         input = s;
         Debug.Log(s);
+    }
+    public void traversal(){
+        string preorder = "";
+        preorder += unityGameObjects[0].GetComponentInChildren<TextMeshPro>().text.ToString();
+        preorder += unityGameObjects[1].GetComponentInChildren<TextMeshPro>().text.ToString();
+        preorder += unityGameObjects[3].GetComponentInChildren<TextMeshPro>().text.ToString();
+        preorder += unityGameObjects[4].GetComponentInChildren<TextMeshPro>().text.ToString();
+        preorder += unityGameObjects[2].GetComponentInChildren<TextMeshPro>().text.ToString();
+        preorder += unityGameObjects[5].GetComponentInChildren<TextMeshPro>().text.ToString();
+        preorder += unityGameObjects[6].GetComponentInChildren<TextMeshPro>().text.ToString();
+        preordertext.text += preorder;
+
+        string inorder = "";
+        inorder += unityGameObjects[3].GetComponentInChildren<TextMeshPro>().text.ToString();
+        inorder += unityGameObjects[1].GetComponentInChildren<TextMeshPro>().text.ToString();
+        inorder += unityGameObjects[4].GetComponentInChildren<TextMeshPro>().text.ToString();
+        inorder += unityGameObjects[0].GetComponentInChildren<TextMeshPro>().text.ToString();
+        inorder += unityGameObjects[5].GetComponentInChildren<TextMeshPro>().text.ToString();
+        inorder += unityGameObjects[2].GetComponentInChildren<TextMeshPro>().text.ToString();
+        inorder += unityGameObjects[6].GetComponentInChildren<TextMeshPro>().text.ToString();
+        inordertext.text += inorder;
+
+
+        string postorder = "";
+        postorder += unityGameObjects[3].GetComponentInChildren<TextMeshPro>().text.ToString();
+        postorder += unityGameObjects[4].GetComponentInChildren<TextMeshPro>().text.ToString();
+        postorder += unityGameObjects[1].GetComponentInChildren<TextMeshPro>().text.ToString();
+        postorder += unityGameObjects[5].GetComponentInChildren<TextMeshPro>().text.ToString();
+        postorder += unityGameObjects[6].GetComponentInChildren<TextMeshPro>().text.ToString();
+        postorder += unityGameObjects[2].GetComponentInChildren<TextMeshPro>().text.ToString();
+        postorder += unityGameObjects[0].GetComponentInChildren<TextMeshPro>().text.ToString();
+        postordertext.text += postorder;
+
+    }
+    public void Add(){
+        
+            if(index == 1){
+                // line.enabled = true;
+                line.material = material;
+
+            }
+            if(index == 2){   
+                // line2.enabled = true;
+                line2.material = material;
+
+            }
+            if(index == 3){
+                line3.material = material;
+                line3.enabled = true;
+   
+            }
+            if(index == 4){
+                line4.material = material;
+                line4.enabled = true;
+   
+            }
+            if(index == 5){
+                line5.material = material;
+                line5.enabled = true;
+   
+            }
+            if(index == 6){
+                line6.material = material;
+                line6.enabled = true;
+                // traversal();
+   
+            }
+            if(index < 7){
+                unityGameObjects[index].SetActive(true);
+                unityGameObjects[index].GetComponentInChildren<TextMeshPro>().text = input.ToString();
+                index++;
+            }
+             
+            
+           
+            
+        
     }
 
 }
---
+++ Assets/circularqueue2.cs
@@ -0,0 +1,97 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class circularqueue2 : MonoBehaviour
+{
+    List<GameObject> unityGameObjects = new List<GameObject>();
+    List<Vector3> posi = new List<Vector3>();
+    public GameObject node0;
+    public GameObject node1;
+    public GameObject node2;
+    public GameObject node3;
+    public GameObject node4;
+    public GameObject node5;
+    public GameObject node6;
+    public GameObject node7;
+    public string input;    
+    public TextMeshProUGUI inputField;
+    public TextMeshProUGUI overflowtext;
+    public TextMeshProUGUI underflowtext;
+    public TextMeshProUGUI fronttext;
+    public Text pushCode;
+    public Text overFlow;
+    public Text popCode;
+    public Text underFlow;
+    int count = 0;
+    int max = 8;
+    int front = 0;
+    int rear = -1;
+
+
+    void Start()
+    {
+        unityGameObjects.Add(node0);
+        unityGameObjects.Add(node1);
+        unityGameObjects.Add(node2);
+        unityGameObjects.Add(node3);
+        unityGameObjects.Add(node4);
+        unityGameObjects.Add(node5);
+        unityGameObjects.Add(node6);
+        unityGameObjects.Add(node7);
+        posi.Add(new Vector3(0,0,0));
+
+    }
+
+    // Update is called once per frame
+    void Update(){
+        if(Input.GetKeyDown(KeyCode.Space)){
+            if(count == max){
+                overflowtext.text = "Overflow!!";
+                popCode.color = Color.white;
+                underFlow.color = Color.white;
+                pushCode.color = Color.white;
+                overFlow.color = Color.green;
+                Debug.Log("Overflow");
+            }else{
+                fronttext.transform.position = posi[0];
+                popCode.color = Color.white;
+                underFlow.color = Color.white;
+                pushCode.color = Color.green;
+                overFlow.color = Color.white;
+                overflowtext.text = " ";
+                underflowtext.text = " ";
+                rear = (rear+1) % max;
+                unityGameObjects[rear].GetComponentInChildren<TextMeshPro>().text = input.ToString();
+                count++;
+            }
+        }
+        if(Input.GetKeyDown(KeyCode.F)){
+            if(count == 0){
+                popCode.color = Color.white;
+                underFlow.color = Color.green;
+                pushCode.color = Color.white;
+                overFlow.color = Color.white;
+                underflowtext.text = "Underflow!!";
+                Debug.Log("underflow");
+            }else{
+                popCode.color = Color.green;
+                underFlow.color = Color.white;
+                pushCode.color = Color.white;
+                overFlow.color = Color.white;
+                overflowtext.text = " ";
+                underflowtext.text = " ";
+                unityGameObjects[front].GetComponentInChildren<TextMeshPro>().text = "";
+                front = (front+1) % max;
+                count--;
+            }
+        }
+
+    }
+    public void GetInput(string s){
+        input = s;
+        Debug.Log(s);
+    }
+}
